1979dfea35dd6464ac1ba7c9805520b94ca0c8ec
juliabrazolim <juliabrazolim@gmail.com>
2020-10-16 19:11:12 -0300

    Conversor de temperatura Celsius para Fahrenheit

---
+++ python/temperatura.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+print('Passar graus Celsius para Fahrenheit')
+C = float(input('Insira o valor da temperatura em graus Celsius: '))
+temp = (C * 9/5) + 32
+print('\n A temperatura em Fahrenheit é de:', round(temp, 1), 'ºF')
